f05953c8fa6aabf28188b3999cde7fc84bec529b
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-06-08 21:31:48 +0800

    [新增] clsx 套件

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './app.module.sass'
+import clsx from 'clsx'
 
 function App() {
+  const [ state ] = useState(true)
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className={clsx(styles.title, state && styles.hideTitle)}>Title</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code className='main-blue'>src/App.js</code> and save to reload.

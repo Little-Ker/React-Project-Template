b2a5643447802422f66e399f0d24f5e12aba30ea
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-06-08 23:15:29 +0800

    [新增] Axios 範例、資料json檔

--- src/view/ViewA.jsx
+++ src/view/ViewA.jsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
+import axios from 'axios'
+
+const AxiosEx = () => {
+    const [data, setData] = React.useState([])
+
+    useEffect(() => {
+        axios.get('/data/dataList.json').then((response) => {
+            setData(response.data.titleData)
+        })
+    }, [])
+
+    return (
+        <div>
+            <h2>Axios</h2>
+            {data.map((item, index) => (
+                <p key={index}>{item.title} : {item.txt}</p>
+            ))}
+        </div>
+    )
+}
 
 const ViewA = () => {
     return (
         <>
             <h1>ViewA</h1>
+            <AxiosEx />
         </>
     )
 }

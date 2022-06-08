b6b234e9bb8c4f943ed41e3c0edf110b46e8484d
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-06-08 22:22:12 +0800

    [新增] eslint 規範與指令

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        // 將關於分號的檢查關掉
+        "semi": ["error", "never"]
+    }
+}

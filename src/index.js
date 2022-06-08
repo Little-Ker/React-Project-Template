a64d8f51f055ca86cc41a1067ff8c458fc75fb4b
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-06-09 00:04:21 +0800

    [修改] redux 環境配置

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './styles/all.sass'
+import { Provider } from "react-redux"
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 )
 
 reportWebVitals()
---
+++ src/redux/todoSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit"
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    // ...
+  },
+  reducers: {
+    // ...
+  },
+})
+ 
+export default todoSlice.reducer
